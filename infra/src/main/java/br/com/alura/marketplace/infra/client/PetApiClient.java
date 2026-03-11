122f6d10dbc268dc187b0f4f08485c78d98e0dfc
Rodrigo Abreu <rodrigodiasdeabreu@gmail.com>
2026-03-11 08:09:48 -0300

    refactor: format code and remove commented-out sections in mapping interfaces

--- infra/src/main/java/br/com/alura/marketplace/infra/client/PetApiClient.java
+++ infra/src/main/java/br/com/alura/marketplace/infra/client/PetApiClient.java
@@ -1,10 +1,9 @@
-//package br.com.alura.marketplace.infra.client;
-//
-//import com.petstore.api.PetApi;
-//import org.springframework.cloud.openfeign.FeignClient;
-//import org.springframework.stereotype.Component;
-//
-//@FeignClient(name = "PetApiClient", url = "${external-api.rest.petstore.url}")
-//public interface PetApiClient extends PetApi {
-//
-//}
+package br.com.alura.marketplace.infra.client;
+
+import com.petstore.api.PetApi;
+import org.springframework.cloud.openfeign.FeignClient;
+
+@FeignClient(name = "PetApiClient", url = "${external-api.rest.petstore.url}")
+public interface PetApiClient extends PetApi {
+
+}
--- infra/src/main/java/br/com/alura/marketplace/infra/map/StringToTagMap.java
+++ infra/src/main/java/br/com/alura/marketplace/infra/map/StringToTagMap.java
@@ -1,12 +1,12 @@
-//package br.com.alura.marketplace.infra.map;
-//
-//import com.petstore.model.TagDto;
-//
-//public interface StringToTagMap {
-//
-//    default TagDto mapStringToTag(String value) {
-//        if (value == null)
-//            return null;
-//        return new TagDto().name(value);
-//    }
-//}
+package br.com.alura.marketplace.infra.map;
+
+import com.petstore.model.TagDto;
+
+public interface StringToTagMap {
+
+    default TagDto mapStringToTag(String value) {
+        if (value == null)
+            return null;
+        return new TagDto().name(value);
+    }
+}
